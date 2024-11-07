10cc84c84541bf155ea5bd16732edb3891dd4639
Developersmz <developersmz12@gmail.com>
2024-11-07 16:59:34 +0200

    Update Models.js

--- src/models/Models.js
+++ src/models/Models.js
@@ -53,12 +53,12 @@ const Answer = db.sequelize.define('Answer', {
     }
 })
 
-db.sequelize.sync({ alter: true })
-    .then(() => {
-        console.log('Banco de dados sincronizado e modelo atualizado.');
-    })
-    .catch((err) => {
-        console.log('Erro ao sincronizar o banco de dados:', err);
-    });
+// db.sequelize.sync({ alter: true })
+//     .then(() => {
+//         console.log('Banco de dados sincronizado e modelo atualizado.');
+//     })
+//     .catch((err) => {
+//         console.log('Erro ao sincronizar o banco de dados:', err);
+//     });
 
 module.exports = { User, About, Answer }
